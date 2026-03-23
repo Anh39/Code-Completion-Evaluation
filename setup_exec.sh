ce3c8e1e4b8c4cab01435449f698f30df6dc5ca5
Anh39 <120157750+Anh39@users.noreply.github.com>
2026-03-23 22:08:35 +0700

    Update setup_exec.sh

--- setup_exec.sh
+++ setup_exec.sh
@@ -1,5 +1,5 @@
-# wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-# bash Miniconda3-latest-Linux-x86_64.sh -b
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh -b
 
 git lfs install
 git clone https://huggingface.co/datasets/CSJianYang/ExecRepoBench
