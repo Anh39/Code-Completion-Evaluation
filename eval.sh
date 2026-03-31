393a5a9073900c039abe3bbd48ac2e6285afb10a
Anh39~ <22022514@vnu.edu.vn>
2026-03-31 08:39:41 +0700

    update bash script

--- eval.sh
+++ eval.sh
@@ -5,16 +5,10 @@ bash total_sim.sh \
     qwen25-05-base \
     0 \
     0.9
-bash total_sim.sh \
-    ../outputs/qwen25-15-base \
-    Qwen/Qwen2.5-Coder-1.5B \
-    qwen25-15-base \
-    0 \
-    0.9
-bash total_sim.sh \
-    ../outputs/qwen25-30-base \
-    Qwen/Qwen2.5-Coder-3B \
-    qwen25-30-base \
+bash total_pass.sh \
+    ../outputs/qwen25-05-base \
+    Qwen/Qwen2.5-Coder-0.5B \
+    qwen25-05-base \
     0 \
     0.9
 cd ..
